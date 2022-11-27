3c3e05cc87a42ab989269bdaea549e50dcb0ab93
Kanyerezi30 <kanyerezi30@gmail.com>
2022-11-27 20:46:59 +0300

    align reads

--- sarscov.sh
+++ sarscov.sh
@@ -12,4 +12,13 @@ printf "\e[4mTrimming poor quality reads...\n\e[0m"
 
 printf "\e[4mIndex the reference genome...\n\e[0m"
 
-bowtie2-build -q --threads 40 -f reference/sequence.fasta reference/sarscovReference
+#bowtie2-build -q --threads 40 -f reference/sequence.fasta reference/sarscovReference
+
+printf "\e[4mAlign the reads to the reference genome...\n\e[0m"
+
+for i in $(ls tmp/*gz | cut -f1 -d"_" | cut -f2 -d"/" | sort -u)
+do
+	R1=$(ls tmp/${i}*R1*gz)
+	R2=$(ls tmp/${i}*R2*gz)
+	bowtie2 -p 40 -x reference/sarscovReference -1 $R1 -2 $R2 > ${i}.sam
+done
