f5cf330a885a041a2f9ccbb0bf41f0b5235db338
Kanyerezi30 <kanyerezi30@gmail.com>
2023-01-02 06:30:22 +0300

    Perform alignment

--- cov19.sh
+++ cov19.sh
@@ -13,3 +13,12 @@ do
 done
 
 bwa index ref/sequence.fasta # index reference
+
+# Perform alignment
+for i in $(ls *val*gz | cut -f1 -d"_" | sort -u)
+do
+        forward=$(ls ${i}*_R1_*val_1*)
+        reverse=$(ls ${i}*_R2_*val_2*)
+        bwa mem -t 20 ref/sequence.fasta $forward $reverse | \
+        samtools sort -o ${i}.sorted.bam
+done
